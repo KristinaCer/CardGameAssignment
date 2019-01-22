78075da84211f1d73dc12a561a072524425cb519
KristinaCer <2094641c@student.gla.ac.uk>
2019-01-22 19:02:59 +0000

    New schema

--- src/main/resources/db/migration/V1.0__CreateTables.sql
+++ src/main/resources/db/migration/V1.0__CreateTables.sql
@@ -1,6 +1,20 @@
+CREATE TABLE player(
+ id serial PRIMARY KEY,
+ name VARCHAR (255) NOT NULL UNIQUE,
+ type VARCHAR (255) NOT NUll
+);
+
+
 CREATE TABLE game_result(
  id serial PRIMARY KEY,
- winner VARCHAR (50)   NOT NULL,
- numberOfRounds INT NOT NULL,
- statistics VARCHAR (355) UNIQUE NOT NULL
+ winner INTEGER REFERENCES player(id),
+ numberOfRounds INT NOT NULL 
+); 
+
+
+CREATE TABLE participation(
+ id serial PRIMARY KEY,
+ roundsWon INTEGER NOT NULL,
+ playerId INTEGER REFERENCES player(id) NOT NULL,
+ gameId INTEGER REFERENCES game_result(id) NOT NULL
 );
